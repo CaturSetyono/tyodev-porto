7cfc9a3a5784fcbe0c52dfae565ae37b40995a71
Catur Setyono <catursetyono542@gmail.com>
2025-07-02 22:07:09 +0700

    delete all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import AppSidebar from '@/components/app-sidebar';
 import Footer from '@/components/Footer';
 const inter = Inter({ subsets: ['latin'] });

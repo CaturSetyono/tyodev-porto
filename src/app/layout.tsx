62256f136636565a425669c82ebcb6da2eac74f4
Catur Setyono <catursetyono542@gmail.com>
2025-08-07 21:35:28 +0700

    style: add Navbar component and enhance HTML structure in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from '@/components/navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -14,8 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
+    <html lang="id" className="scroll-smooth">
       <body className={`${inter.className} bg-gray-50 dark:bg-gray-900`}>
+          <Navbar />
           <main className="flex-grow">
             {children}
           </main>

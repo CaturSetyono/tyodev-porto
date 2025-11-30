f0306e1b0b5e638845fcf4d0fbda68ac8926eb6f
Catur Setyono <catursetyono542@gmail>
2025-11-30 22:13:53 +0700

    resolve first conflict in branch master

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-<<<<<<< HEAD
-import './globals.css';
-import Navbar from '@/components/navbar';
-=======
->>>>>>> 1a626899b69d963c2c44e779e47e77084dc01a24
-const inter = Inter({ subsets: ['latin'] });
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Catur Setyono - Web Developer',
-  description: 'Website portofolio pribadi dibuat oleh Catur Setyono',
+  title: "Catur Setyono - Web Developer",
+  description: "Website portofolio pribadi dibuat oleh Catur Setyono",
 };
 
 export default function RootLayout({
@@ -18,18 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-<<<<<<< HEAD
     <html lang="id" className="scroll-smooth">
       <body className={`${inter.className} bg-gray-50 dark:bg-gray-900`}>
-          <Navbar />
-          <main className="flex-grow">
-            {children}
-          </main>
-=======
-    <html lang="id">
-      <body className={`${inter.className} bg-gray-50 dark:bg-gray-900`}> {/* Tambahkan warna latar default */}
-        {children}
->>>>>>> 1a626899b69d963c2c44e779e47e77084dc01a24
+        <Navbar />
+        <main className="flex-grow">{children}</main>
       </body>
     </html>
   );
